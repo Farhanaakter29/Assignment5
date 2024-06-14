615cf94241945567411794abd280e9984cc69794
farhana akter <farhanaakter120223@gmail.com>
2024-06-14 22:52:49 +0700

    E-6.9

---
+++ src/Streing2.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class Streing2{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a word:");
+        String word = scanner.nextLine();
+        StringBuilder reversedWord = new StringBuilder(word);
+
+        reversedWord.reverse();
+        System.out.println("Reversed word: " + reversedWord.toString());
+
+        scanner.close();
+    }
+}
